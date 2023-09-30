053e0306b52e7e3174d75716645ab095b78ee409
rescobarm <rudyard.matuz@hotmail.com>
2023-09-30 17:02:08 -0600

    Remove appsettings.json from version control 01

--- MakeMyMoney/Program.cs
+++ MakeMyMoney/Program.cs
@@ -23,15 +23,23 @@ builder.Services.AddScoped<IWebTokenRepository, WebTokenRepository>();
 
 //UserFireBaseWSSrvc : ISignUpSrvc
 builder.Services.AddScoped<IUserFireBaseWSSrvc, UserFireBaseWSSrvc>();
+builder.Services.AddSingleton<ConfigurationService>();
+//builder.Services.Configure<AppSettings>(config =>
+//{
+//    //var configuration = new ConfigurationBuilder()
+//    //    .AddJsonFile("appsettings.json") // Asegúrate que este archivo se copie al directorio de salida
+//    //    .AddJsonFile($"appsettings.{env.Environment}.json", optional: true, reloadOnChange: true)
+//    //    .Build();
 
-builder.Services.Configure<AppSettings>(config =>
-{
-    var configuration = new ConfigurationBuilder()
-        .AddJsonFile("appsettings.json") // Asegúrate que este archivo se copie al directorio de salida
-        .Build();
+//    /*
+//     _configuration = new ConfigurationBuilder()
+//            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+//            .AddJsonFile($"appsettings.{env.Environment}.json", optional: true, reloadOnChange: true)
+//            .Build();
+//    */
 
-    config.FirebaseKey = configuration["FirebaseKey"];
-});
+//    config.FirebaseKey = configuration["FirebaseKey"];
+//});
 
 builder.Services.AddHttpClient("HTTPCFirebase", client => {
     client.BaseAddress = new Uri("https://identitytoolkit.googleapis.com/");
@@ -39,3 +47,21 @@ builder.Services.AddHttpClient("HTTPCFirebase", client => {
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 //builder.Services.AddHttpClient();
 await builder.Build().RunAsync();
+
+public class ConfigurationService
+{
+    private readonly IConfiguration _configuration;
+
+    public ConfigurationService(IWebAssemblyHostEnvironment env)
+    {
+        _configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{env.Environment}.json", optional: true, reloadOnChange: true)
+            .Build();
+    }
+
+    public string GetFirebaseKey()
+    {
+        return _configuration["FirebaseKey"];
+    }
+}
